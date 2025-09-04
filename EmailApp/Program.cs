5fbffe566971f6bfb378ee1723430cbebba542a6
erhangndz <erhangndz@live.com>
2025-09-04 23:09:42 +0300

    Add project files.

---
+++ EmailApp/Program.cs
@@ -0,0 +1,50 @@
+using EmailApp.Context;
+using EmailApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+
+
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddIdentity<AppUser, AppRole>(config =>
+{
+    config.User.RequireUniqueEmail = true;
+    
+}).AddEntityFrameworkStores<AppDbContext>();
+
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
